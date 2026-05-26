0460fde8ee52ad03dc5ec33c5280a8aec12f2ffc
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 16:51:10 -0500

    installed browser router and created navigation in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <h1>Practical Bookshelf</h1>
+
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/library'>My Library</Link>
+        <Link to='/current-reads'>Current Reads</Link>
+        <Link to='/to-read'>To Read</Link>
+      </div>
+    </nav>
   )
 }
