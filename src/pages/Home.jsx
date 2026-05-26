5d70214c6c4e4be226096aec3ec667e6d1038f23
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 16:55:03 -0500

    ome.jsx link to Navbar.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+
+      <h1>Welcome to Practical Bookshelf</h1>
+
+      <p>Track your home library and reading progress.</p>
+    </div>
   )
 }
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Library() {
   return (
     <div>Library</div>
