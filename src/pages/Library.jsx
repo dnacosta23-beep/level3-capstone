4b9b2bf2713fc14dd649d41f89468a2836038b2e
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 17:44:02 -0500

    update the map in Library.jsx with the props from BookCard.

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -3,9 +3,9 @@ import Navbar from '../components/Navbar'
 import { supabase } from '../utils/supabase'
 
 export default function Library() {
-const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([])
 
-useEffect(() => {
+  useEffect(() => {
     fetchBooks()
   }, [])
 
@@ -20,18 +20,20 @@ useEffect(() => {
       setBooks(data)
     }
   }
-  
+
   return (
-        <div>
+    <div>
       <Navbar />
 
       <h1>My Library</h1>
 
-      {books.map((book) => (
-        <div key={book.id}>
-          <h2>{book.title}</h2>
-          <p>{book.author}</p>
-        </div>
+       {books.map((book) => (
+        <BookCard
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          status={book.status}
+        />
       ))}
     </div>
   )
