67d4922b35722df66f9f16c213033e309dd05a79
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 16:13:46 -0500

    Empty start up files and folders created, supabase linked in.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Welcome to the Level 3 Capstone Project!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/CurrentReads.jsx
@@ -0,0 +1,5 @@
+export default function CurrentReads() {
+  return (
+    <div>CurrentReads</div>
+  )
+}
