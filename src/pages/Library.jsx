2a96d998d6c6f4664996d1bd707068abfe367414
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 16:58:54 -0500

    In Library.jex, set up for useState and useEffect, set up mapping.

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import { supabase } from '../utils/supabase'
 
 export default function Library() {
+const [books, setBooks] = useState([])
+
   return (
-    <div>Library</div>
+        <div>
+      <Navbar />
+
+      <h1>My Library</h1>
+
+      {books.map((book) => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+        </div>
+      ))}
+    </div>
   )
 }
