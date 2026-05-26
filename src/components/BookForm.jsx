27b8379ce1137221f0217875125011a673c77166
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 17:19:35 -0500

    set up for useState and create the form input and create button.

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+import { supabase } from '../utils/supabase'
+
 export default function BookForm() {
+const [title, setTitle] = useState('')
+const [author, setAuthor] = useState('')
+
+
   return (
-    <div>BookForm</div>
+       <form onSubmit={addBook}>
+      <input
+        type='text'
+        placeholder='Book Title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        type='text'
+        placeholder='Author'
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+
+      <button type='submit'>Add Book</button>
+    </form>
   )
 }
