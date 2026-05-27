3cb664908668366d6fc4717fd80966f1d5989f9a
Danielle Acosta <dnacosta23@gmail.com>
2026-05-26 19:09:46 -0500

    Updated the BookCard prop in the BookCard.jsx. Added the function prop current_page and updateProgress. Added the input and button for updating the progress.

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,30 @@
 
-export default function BookCard({ id, title, author, status, deleteBook }) {
+export default function BookCard({ id,
+  title,
+  author,
+  status,
+  current_page,
+  deleteBook,
+  updateProgress 
+}) {
+const [currentPage, setCurrentPage] = useState(current_page || 0)
+
   return (
   <div>
       <h2>{title}</h2>
       <p>{author}</p>
       <span>{status}</span>
+
+    <input
+        type='number'
+        value={currentPage}
+        onChange={(e) =>
+          setCurrentPage(e.target.value)
+        }
+      />
+        <button onClick={() => updateProgress(id, currentPage)}>
+        Update Progress
+      </button>
 
       <button onClick={() => deleteBook(id)}>
         Delete
