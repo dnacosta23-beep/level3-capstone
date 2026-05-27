9e425e1516fa0d6fdc5ab507f97aab6ac8793ec3
Danielle Acosta <dnacosta23@gmail.com>
2026-05-27 11:10:51 -0500

    Filled in CurrentReads.jsx with fetchBook, deleteBook, and UpdateProgress functions. Added mapping to return.

--- src/pages/CurrentReads.jsx
+++ src/pages/CurrentReads.jsx
@@ -1,12 +1,66 @@
-import Navbar from '../components/Navbar'
+
 import Sidebar from '../components/Sidebar'
+import BookCard from '../components/BookCard'
+
+import { useEffect, useState } from 'react'
+import { supabase } from '../utils/supabase'
 
 export default function CurrentReads() {
+ const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+ async function fetchBooks() {
+    const {data: { user }} = await supabase.auth.getUser()
+
+    const { data, error } = await supabase.from('books').select('*').eq('user_id', user.id).eq('status', 'Currently Reading')
+
+    if (error) {
+      console.log(error)
+    } else {
+      setBooks(data)
+    }
+  }
+
+  async function deleteBook(id) {
+    const { error } = await supabase.from('books').delete().eq('id', id)
+
+    if (error) {
+      console.log(error)
+    } else {
+      fetchBooks()
+    }
+  }
+
+  async function updateProgress(id, page) {
+    const { error } = await supabase.from('books').update({current_page: page}).eq('id', id)
+
+    if (error) {
+      console.log(error)
+    } else {
+      fetchBooks()
+    }
+  }
+
   return (
     <div>
-        <Navbar />
         <Sidebar />
-        CurrentReads
+        <h1>Current Reads</h1>
+
+        {books.map((book) => (
+            <BookCard
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              author={book.author}
+              status={book.status}
+              current_page={book.current_page}
+              deleteBook={deleteBook}
+              updateProgress={updateProgress}
+            />
+          ))}
     </div>
   )
 }
